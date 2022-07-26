bb78240c2ccc159eb49c79e3f2c61586ee6e6714
microft <microft4959@gmail.com>
2022-07-26 17:29:31 +0800

    7/26下午

--- service/service_hosp/src/main/java/jw/hospital/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/jw/hospital/yygh/hosp/controller/HospitalSetController.java
@@ -4,7 +4,7 @@ package jw.hospital.yygh.hosp.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import jw.hospital.yygh.common.utils.MD5;
+//import jw.hospital.yygh.common.utils.MD5;
 import jw.hospital.yygh.hosp.service.HospitalSetService;
 import jw.hospital.yygh.model.hosp.HospitalSet;
 import jw.hospital.yygh.common.result.Result;
@@ -12,6 +12,7 @@ import jw.hospital.yygh.vo.hosp.HospitalSetQueryVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import sun.plugin2.main.server.ResultID;
 
 import java.util.List;
 import java.util.Random;
@@ -57,19 +58,69 @@ public class HospitalSetController {
         return Result.ok(hospitalSetService.page(page,wrapper));
     }
 
-    @ApiOperation(value = "添加医院设置")
-    @PostMapping("saveHospitalSet")
-    public Result saveHospSet(@RequestBody HospitalSet hospitalSet){
-        //设置状态 1 使用 0 不能使用
-        hospitalSet.setStatus(1);
-        Random random = new Random();
-        MD5.encrypt(System.currentTimeMillis()+""+random.nextInt(1000));
+//    @ApiOperation(value = "添加医院设置")
+//    @PostMapping("saveHospitalSet")
+//    public Result saveHospSet(@RequestBody HospitalSet hospitalSet){
+//        //设置状态 1 使用 0 不能使用
+//        hospitalSet.setStatus(1);
+//        Random random = new Random();
+//        MD5.encrypt(System.currentTimeMillis()+""+random.nextInt(1000));
+//
+//        boolean save = hospitalSetService.save(hospitalSet);
+//        if(save){
+//            return Result.ok();
+//        } else {
+//            return Result.fail();
+//        }
+//    }
 
-        boolean save = hospitalSetService.save(hospitalSet);
-        if(save){
+    @ApiOperation(value = "根据IP获取医院设置")
+    @GetMapping("getHospSet/{id}")
+    public Result getHospSet(@PathVariable Long id){
+        //设置状态 1 使用 0 不能使用
+        HospitalSet hospitalSet = hospitalSetService.getById(id);
+        return Result.ok(hospitalSet);
+    }
+
+    @ApiOperation(value = "修改医院设置")
+    @PostMapping("updateHospitalSet")
+    public Result updateHospitalSet(@RequestBody HospitalSet hospitalSet){
+        boolean flag = hospitalSetService.updateById(hospitalSet);
+
+        if(flag){
             return Result.ok();
         } else {
             return Result.fail();
         }
+    }
+
+    @ApiOperation(value = "批量删除医院设置")
+    @DeleteMapping("batchRemoveHospitalSet")
+    public Result batchRemoveHospitalSet(@RequestBody List<Long> idList){
+        hospitalSetService.removeByIds(idList);
+        return Result.ok();
+    }
+
+    @ApiOperation(value = "设置锁定和解锁")
+    @PutMapping("lockHospitalSet/{id}/{status}")
+    public Result lockHospitalSet(@PathVariable Long id,
+                                  @PathVariable Integer status){
+        //根据id查询医院设置信息
+        HospitalSet hospitalSet = hospitalSetService.getById(id);
+        //设置状态
+        hospitalSet.setStatus(status);
+        hospitalSetService.updateById(hospitalSet);
+        return Result.ok();
+    }
+
+    @ApiOperation(value = "发送签名密钥")
+    @PutMapping("sendKey/{id}")
+    public Result sendKey(@PathVariable Long id){
+        //根据id查询医院设置信息
+        HospitalSet hospitalSet = hospitalSetService.getById(id);
+        String key = hospitalSet.getSignKey();
+        String hoscode = hospitalSet.getHoscode();
+        //TODO 发送短信
+        return Result.ok();
     }
 }
